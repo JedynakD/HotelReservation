5a6e35760a45e6b0c86b0e88d8ba21d9446760fa
JedynakD <djedynak90@gmail.com>
2015-03-05 13:57:21 +0100

    added tests

--- src/main/java/org/jedynakd/Airline.java
+++ src/main/java/org/jedynakd/Airline.java
@@ -1,10 +1,7 @@
 package org.jedynakd;
 
-/**
- * Created by Damian on 2015-03-04.
- */
 public class Airline implements Observer {
-    boolean isAvailable;
+   private  boolean isAvailable;
 
     public void update(int prize, boolean isAvailable) {
         this.isAvailable = isAvailable;
